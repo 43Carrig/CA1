ec068d94a727398cd0abb157b97142c05fff477c
Sean <sean.byrne@students.ittralee.ie>
2019-02-09 22:12:31 +0000

    Select/Click working successfully

--- Assets/Scripts/MultiTouch.cs
+++ Assets/Scripts/MultiTouch.cs
@@ -9,12 +9,12 @@ public class MultiTouch : MonoBehaviour
 {
     void Update () 
     {
-        Touch myTouch = Input.GetTouch(0);
+        /*Touch myTouch = Input.GetTouch(0);
 
         Touch[] myTouches = Input.touches;
         for(int i = 0; i < Input.touchCount; i++)
         {
             //Do something with the touches
-        }
+        }*/
     }
 }
