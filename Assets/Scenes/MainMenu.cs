51ed71c39c278495293ffae123f43bb3af991709
Sean <sean.byrne@students.ittralee.ie>
2019-02-14 11:43:49 +0000

    Added touchControl and Manipulate classes

--- Assets/Scenes/MainMenu.cs
+++ Assets/Scenes/MainMenu.cs
@@ -5,13 +5,16 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    // Starting the game. Changing the scene from current scene to next scene. 
     public void PlayGame()
     {
+        // Changing the main game scene
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
     public void QuitGame()
     {
+        // This will be to change the application
         Debug.Log("QUIT!");
         Application.Quit();
     }
